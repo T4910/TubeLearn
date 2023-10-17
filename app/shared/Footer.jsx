1832a0a1479a03c1463a2fc53fb5bdc8a37043e0
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2023-10-17 01:52:22 +0100

    Added contributors

--- app/shared/Footer.jsx
+++ app/shared/Footer.jsx
@@ -1,13 +1,15 @@
 import Logo from "./Logo"
 import IconHolder from "./Icon"
 import { FaInstagram, FaLinkedin } from "react-icons/fa"
+import Link from 'next/link'
 
 
 const Footer = () => {
     const currentYear = new Date().getFullYear()
 
     return (
-        <div className='w-full flex items-center justify-between pt-12 pb-5'>
+      <div>
+        <div className='w-full flex items-center justify-between pt-12 pb-2'>
           <Logo/>
           <p className='text-lightGrey'>@ {currentYear} TubeLearn</p>
           <div className='flex items-center space-x-2 '>
@@ -15,6 +17,10 @@ const Footer = () => {
             <IconHolder icon={<FaLinkedin/>} link="#"/>            
           </div>
         </div>
+        <div>
+          <p className="text-center text-lightGrey text-xs">Original source: <Link className="text-orangeDark underline" href="github.com/TubeLearn">TubeLearn Repo</Link> </p>
+        </div>
+      </div>
     )
 }
 
